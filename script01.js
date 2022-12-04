c9e05a9b55b8c5cbfa0e8e327f1fe399f95eb724
Filipe Luis dos Santos <filipe.luisdossantos@gmail.com>
2022-12-04 09:13:55 -0300

    Criando Repositório Git e GitHub

---
+++ script01.js
@@ -0,0 +1,59 @@
+/* Comandos básicos do cmd(prompt de comando) do Windows.
+
+Entrar numa pasta:
+
+        cd [nomePasta]
+
+Listar arquivos:
+
+        dir
+
+Voltar um diretório:
+
+        cd ..
+
+Criar um diretório:
+
+        mkdir [nomePasta]
+
+
+/////////////////////////////////////////////////////////////////
+
+Iniciar o Git na pasta do projeto pelo terminal: (Irá criar um repositório e arquivos do git).
+
+        git init
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
