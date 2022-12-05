68335afb7190200c65638996c4f243fdb627a2ed
Filipe Luis dos Santos <filipe.luisdossantos@gmail.com>
2022-12-05 09:24:26 -0300

    Aprendento a realizar Commit pelo terminal

--- script01.js
+++ script01.js
@@ -24,7 +24,18 @@ Iniciar o Git na pasta do projeto pelo terminal: (Irá criar um repositório e a
         git init
 
 
+Criando um arquivo readme.md (abrir o vscode e criar um arquivo na pasta do projeto readme.md e colocar os textos)
 
+no terminal na pasta do projeto digitar o código abaixo.
+
+        git status                        //realiza uma leitura e verifica mudanças.
+        git add -A                        //adiciona todos os arquivos novos não monitorado pelo git.
+        git commit -m "Criando Projeto"   // realizando o commit
+
+
+caso quisermos verificar o histórico dos commit:
+
+        git log
 
 
 
