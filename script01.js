0c13ec3e626ba19224a7b314dd7f44f31dfbef1f
Filipe Luis dos Santos <filipe.luisdossantos@gmail.com>
2022-12-06 18:53:21 -0300

    atualizando comandos para add e exclusão de commit pelo terminal

--- script01.js
+++ script01.js
@@ -33,14 +33,31 @@ no terminal na pasta do projeto digitar o código abaixo.
         git commit -m "Criando Projeto"   // realizando o commit
 
 
+////////////////////////////////////////////////////////////////
+
+
 caso quisermos verificar o histórico dos commit:
 
         git log
 
 
+/////////////////////////////////////////////////////////////////
+
+Consultar quantos branchs temos no projeto.
+
+        git branch    // o que tiver o * é o que estamos atualmente.
 
 
+Voltar para um commit anterior.
 
+        git log    //pegar o id do commit que queremos voltar.
+        
+        
+        git reset --soft [colar id commit] //voltar para commit especif. mantendo as alterações atuais, necess. dar novo commit
+
+ou
+
+        git reset --hard [colar id commit]   //voltar para o commit especificado resetando todas as alterações do atual.
 
 
 
